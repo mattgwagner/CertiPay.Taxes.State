6380a35f64b42205b1f89c21d55e0dc6b82827f8
Matt Wagner <mattgwagner@gmail.com>
2015-10-16 11:24:03 -0400

    Drop in build scripts from other OSS projects

--- Build-Package.ps1
+++
@@ -1,5 +0,0 @@
-$BaseDir = Split-Path (Resolve-Path $MyInvocation.MyCommand.Path)
-
-mkdir "$BaseDir\Artifacts" -Force
-
-& "$BaseDir\.NuGet\NuGet.exe" Pack CertiPay.Taxes.State\CertiPay.Taxes.State.nuspec -Properties Configuration=Release -OutputDirectory "$BaseDir\artifacts\"
