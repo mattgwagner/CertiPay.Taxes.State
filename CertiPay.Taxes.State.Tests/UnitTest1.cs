81c2e5833de26e00b5677d90a3f95d7b05dd1714
Kyle Jones <kylebjones09@gmail.com>
2016-01-15 13:41:13 -0500

    Added Test project

---
+++ CertiPay.Taxes.State.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CertiPay.Taxes.State.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
