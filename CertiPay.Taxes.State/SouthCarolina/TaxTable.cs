34f9100698058fe353aad4a2148084d0d7ab5efe
Matt Wagner <mattgwagner@gmail.com>
2016-04-21 20:49:25 -0400

    Initial spike on SC taxes

---
+++ CertiPay.Taxes.State/SouthCarolina/TaxTable.cs
@@ -0,0 +1,38 @@
+﻿using CertiPay.Payroll.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CertiPay.Taxes.State.SouthCarolina
+{
+    public abstract class TaxTable
+    {
+        public virtual Decimal Calculate(Decimal grossWages, PayrollFrequency frequency, int exemptions = 0)
+        {
+            var annualized_wages = frequency.CalculateAnnualized(grossWages) - (exemptions * ExemptionValue);
+
+            var tax_table =
+                Table
+                .Where(row => row.StartingAmount <= annualized_wages)
+                .Where(row => row.MaximumWage > annualized_wages)
+                .Single();
+
+            return tax_table.TaxBase + (annualized_wages - tax_table.StartingAmount) * tax_table.TaxRate;
+        }
+
+        public abstract Decimal ExemptionValue { get; }
+
+        public abstract IEnumerable<TableRow> Table { get; }
+
+        public class TableRow
+        {
+            public Decimal TaxBase { get; set; }
+
+            public Decimal StartingAmount { get; set; }
+
+            public Decimal MaximumWage { get; set; }
+
+            public Decimal TaxRate { get; set; }
+        }
+    }
+}
