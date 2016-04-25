d888625e658ca5d87631c9d183b0b7108e7fee6b
Matt Wagner <mattgwagner@gmail.com>
2016-04-25 10:16:55 -0400

    Add additional test cases for NC
    [skip ci]

--- CertiPay.Taxes.State.Tests/NC/TaxTable2016Tests.cs
+++ CertiPay.Taxes.State.Tests/NC/TaxTable2016Tests.cs
@@ -11,6 +11,12 @@ namespace CertiPay.Taxes.State.Tests.NC
         [TestCase(PayrollFrequency.Weekly, 450, EmployeeTaxFilingStatus.Single, 2, 12)]
         [TestCase(PayrollFrequency.BiWeekly, 1000, EmployeeTaxFilingStatus.Single, 1, 35)]
         [TestCase(PayrollFrequency.SemiMonthly, 1000, EmployeeTaxFilingStatus.Single, 1, 34)]
+        [TestCase(PayrollFrequency.Weekly, 450, EmployeeTaxFilingStatus.MarriedFilingJointly, 2, 12)]
+        [TestCase(PayrollFrequency.BiWeekly, 1000, EmployeeTaxFilingStatus.MarriedFilingJointly, 1, 35)]
+        [TestCase(PayrollFrequency.SemiMonthly, 1000, EmployeeTaxFilingStatus.MarriedFilingJointly, 1, 34)]
+        [TestCase(PayrollFrequency.Weekly, 450, EmployeeTaxFilingStatus.HeadOfHousehold, 2, 7)]
+        [TestCase(PayrollFrequency.BiWeekly, 1000, EmployeeTaxFilingStatus.HeadOfHousehold, 1, 25)]
+        [TestCase(PayrollFrequency.SemiMonthly, 1000, EmployeeTaxFilingStatus.HeadOfHousehold, 1, 22)]
         public void Checks_And_Balances(PayrollFrequency frequency, Decimal grossWages, EmployeeTaxFilingStatus taxStatus, int allowances, Decimal expected)
         {
             var result = new NorthCarolina.TaxTable2016().Calculate(grossWages, taxStatus, frequency, allowances);
