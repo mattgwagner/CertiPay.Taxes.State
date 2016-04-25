1f04a57cd88c5b5ab80f73b0d54be2a78ec70504
Matt Wagner <mattgwagner@gmail.com>
2016-04-25 14:34:16 -0400

    Add common filing status enum

---
+++ CertiPay.Taxes.State/FilingStatus.cs
@@ -0,0 +1,14 @@
+﻿namespace CertiPay.Taxes.State
+{
+    /// <summary>
+    /// Describes a common, shared filing status used across many states
+    /// </summary>
+    public enum FilingStatus : byte
+    {
+        Single = 0,
+
+        Married = 1,
+
+        Exempt = byte.MaxValue
+    }
+}
