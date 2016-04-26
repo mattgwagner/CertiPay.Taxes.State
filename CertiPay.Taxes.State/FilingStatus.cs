fa26dc30154e3f4aefee6b326d602e0bb90c417c
Matt Wagner <mattgwagner@gmail.com>
2016-04-26 15:55:18 -0400

    Only return filing status if there is withholding

--- CertiPay.Taxes.State/FilingStatus.cs
+++ CertiPay.Taxes.State/FilingStatus.cs
@@ -20,67 +20,72 @@ namespace CertiPay.Taxes.State
     {
         public static IEnumerable<String> ForState(StateOrProvince state)
         {
-            switch (state)
+            if (state.HasWithholding())
             {
-                // TODO Louisiana, West Virginia, Mississippi, Kansas, Deleware, Conneticut, New Jersey, Missouri
+                switch (state)
+                {
+                    // TODO Louisiana, West Virginia, Mississippi, Kansas, Deleware, Conneticut, New Jersey, Missouri
 
-                case StateOrProvince.AL:
-                    yield return "No Exemption";
-                    yield return "Single Exemption";
-                    yield return "Head of Family";
-                    yield return "Married Filing Separate";
-                    yield return "Married Filing Joint";
-                    break;
+                    case StateOrProvince.AL:
+                        yield return "No Exemption";
+                        yield return "Single Exemption";
+                        yield return "Head of Family";
+                        yield return "Married Filing Separate";
+                        yield return "Married Filing Joint";
+                        break;
 
-                case StateOrProvince.OH:
-                case StateOrProvince.MI:
-                case StateOrProvince.KY:
-                case StateOrProvince.PA:
-                    yield return "Normal";
-                    yield return "% of Gross";
-                    break;
+                    case StateOrProvince.OH:
+                    case StateOrProvince.MI:
+                    case StateOrProvince.KY:
+                    case StateOrProvince.PA:
+                        yield return "Normal";
+                        yield return "% of Gross";
+                        break;
 
-                case StateOrProvince.GA:
-                    yield return Georgia.FilingStatus.Single.ToString();
-                    yield return Georgia.FilingStatus.MarriedWithOneIncome.ToString();
-                    yield return Georgia.FilingStatus.MarriedWithTwoIncomes.ToString();
-                    yield return Georgia.FilingStatus.MarriedFilingSeparate.ToString();
-                    yield return Georgia.FilingStatus.HeadOfHousehold.ToString();
-                    break;
+                    case StateOrProvince.GA:
+                        yield return Georgia.FilingStatus.Single.ToString();
+                        yield return Georgia.FilingStatus.MarriedWithOneIncome.ToString();
+                        yield return Georgia.FilingStatus.MarriedWithTwoIncomes.ToString();
+                        yield return Georgia.FilingStatus.MarriedFilingSeparate.ToString();
+                        yield return Georgia.FilingStatus.HeadOfHousehold.ToString();
+                        break;
 
-                case StateOrProvince.NC:
-                case StateOrProvince.CA:
-                case StateOrProvince.MA:
-                    yield return NorthCarolina.FilingStatus.Single.ToString();
-                    yield return NorthCarolina.FilingStatus.Married.ToString();
-                    yield return NorthCarolina.FilingStatus.HeadOfHousehold.ToString();
-                    break;
+                    case StateOrProvince.NC:
+                    case StateOrProvince.CA:
+                    case StateOrProvince.MA:
+                        yield return NorthCarolina.FilingStatus.Single.ToString();
+                        yield return NorthCarolina.FilingStatus.Married.ToString();
+                        yield return NorthCarolina.FilingStatus.HeadOfHousehold.ToString();
+                        break;
 
-                case StateOrProvince.NE:
-                case StateOrProvince.OK:
-                case StateOrProvince.NM:
-                case StateOrProvince.MD:
-                case StateOrProvince.CO:
-                case StateOrProvince.ND:
-                case StateOrProvince.MN:
-                case StateOrProvince.ME:
-                case StateOrProvince.VT:
-                case StateOrProvince.NY:
-                case StateOrProvince.OR:
-                case StateOrProvince.UT:
-                case StateOrProvince.ID:
-                case StateOrProvince.HI:
-                case StateOrProvince.WI:
-                    yield return FilingStatus.Single.ToString();
-                    yield return FilingStatus.Married.ToString();
-                    break;
+                    case StateOrProvince.NE:
+                    case StateOrProvince.OK:
+                    case StateOrProvince.NM:
+                    case StateOrProvince.MD:
+                    case StateOrProvince.CO:
+                    case StateOrProvince.ND:
+                    case StateOrProvince.MN:
+                    case StateOrProvince.ME:
+                    case StateOrProvince.VT:
+                    case StateOrProvince.NY:
+                    case StateOrProvince.OR:
+                    case StateOrProvince.UT:
+                    case StateOrProvince.ID:
+                    case StateOrProvince.HI:
+                    case StateOrProvince.WI:
+                        yield return FilingStatus.Single.ToString();
+                        yield return FilingStatus.Married.ToString();
+                        break;
 
-                default:
-                    yield return "Normal";
-                    break;
+                    default:
+                        yield return "Normal";
+                        break;
+                }
+
+                yield return FilingStatus.Exempt.ToString();
             }
 
-            yield return FilingStatus.Exempt.ToString();
+            
         }
     }
 }
