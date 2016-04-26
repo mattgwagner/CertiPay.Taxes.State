996309a5db1e602eac9ea003bb8f307ef4fd201e
Matt Wagner <mattgwagner@gmail.com>
2016-04-26 11:11:28 -0400

    Add lame test to generate list of filing statuses for regression testing

---
+++ CertiPay.Taxes.State.Tests/FilingStatusTests.cs
@@ -0,0 +1,24 @@
+﻿using CertiPay.Common.Testing;
+using CertiPay.Payroll.Common;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace CertiPay.Taxes.State.Tests
+{
+    public class FilingStatusTests
+    {
+        [Test, Unit]
+        public void Generate_Status_List()
+        {
+            var list = new Dictionary<StateOrProvince, IEnumerable<String>>();
+
+            foreach (var state in States.Values())
+            {
+                list.Add(state, FilingStatuses.ForState(state));
+            }
+
+            list.VerifyMe();
+        }
+    }
+}
