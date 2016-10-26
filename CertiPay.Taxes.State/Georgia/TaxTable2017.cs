f3edbc9afd5425ec6aee07cb0fefc8afe635cbab
Matt Wagner <mattgwagner@gmail.com>
2016-10-26 15:16:35 -0400

    Copy templated tables for GA NC SC

---
+++ CertiPay.Taxes.State/Georgia/TaxTable2017.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CertiPay.Taxes.State.Georgia
+{
+    public class TaxTable2017 : TaxTable
+    {
+        public override int Year { get { return 2017; } }
+
+        public override Decimal SUI_Wage_Base { get { return 9500; } }
+
+        public override IEnumerable<TaxTable.StandardDeduction> StandardDeductions
+        {
+            get
+            {
+                yield return new TaxTable.StandardDeduction { FilingStatus = FilingStatus.MarriedWithOneIncome, Amount = 3000 };
+
+                yield return new TaxTable.StandardDeduction { FilingStatus = FilingStatus.Single, Amount = 2300 };
+                yield return new TaxTable.StandardDeduction { FilingStatus = FilingStatus.HeadOfHousehold, Amount = 2300 };
+
+                yield return new TaxTable.StandardDeduction { FilingStatus = FilingStatus.MarriedWithTwoIncomes, Amount = 1500 };
+                yield return new TaxTable.StandardDeduction { FilingStatus = FilingStatus.MarriedFilingSeparate, Amount = 1500 };
+            }
+        }
+
+        public override IEnumerable<TaxTable.StandardDeduction> PersonalAllowances
+        {
+            get
+            {
+                yield return new TaxTable.StandardDeduction { FilingStatus = FilingStatus.Single, Amount = 2700 };
+                yield return new TaxTable.StandardDeduction { FilingStatus = FilingStatus.HeadOfHousehold, Amount = 2700 };
+
+                yield return new TaxTable.StandardDeduction { FilingStatus = FilingStatus.MarriedWithOneIncome, Amount = 3700 };
+                yield return new TaxTable.StandardDeduction { FilingStatus = FilingStatus.MarriedWithTwoIncomes, Amount = 3700 };
+                yield return new TaxTable.StandardDeduction { FilingStatus = FilingStatus.MarriedFilingSeparate, Amount = 3700 };
+            }
+        }
+
+        public override TaxTable.DependentAllowance DependentAllowances
+        {
+            get { return new TaxTable.DependentAllowance { Amount = 3000 }; }
+        }
+
+        public override IEnumerable<TaxableWithholding> TaxableWithholdings
+        {
+            get
+            {
+                // Percentage method for computing tax on wages subject to withholding
+
+                // Single Individual
+
+                yield return new TaxTable.TaxableWithholding { FilingStatus = FilingStatus.Single, TaxBase = 0.00m, StartingAmount = 0.00m, MaximumWage = 750.00m, TaxRate = .01m };
+                yield return new TaxTable.TaxableWithholding { FilingStatus = FilingStatus.Single, TaxBase = 7.50m, StartingAmount = 750.00m, MaximumWage = 2250.00m, TaxRate = .02m };
+                yield return new TaxTable.TaxableWithholding { FilingStatus = FilingStatus.Single, TaxBase = 37.50m, StartingAmount = 2250.00m, MaximumWage = 3750.00m, TaxRate = .03m };
+                yield return new TaxTable.TaxableWithholding { FilingStatus = FilingStatus.Single, TaxBase = 82.50m, StartingAmount = 3750.00m, MaximumWage = 5250.00m, TaxRate = .04m };
+                yield return new TaxTable.TaxableWithholding { FilingStatus = FilingStatus.Single, TaxBase = 142.50m, StartingAmount = 5250.00m, MaximumWage = 7000.00m, TaxRate = .05m };
+                yield return new TaxTable.TaxableWithholding { FilingStatus = FilingStatus.Single, TaxBase = 230.00m, StartingAmount = 7000.00m, MaximumWage = Decimal.MaxValue, TaxRate = .06m };
+
+                // Married Filing Joint Return (with one spouse having income) OR Head of Household
+
+                yield return new TaxTable.TaxableWithholding { FilingStatus = FilingStatus.HeadOfHousehold, TaxBase = 0.00m, StartingAmount = 0.00m, MaximumWage = 1000.00m, TaxRate = .01m };
+                yield return new TaxTable.TaxableWithholding { FilingStatus = FilingStatus.HeadOfHousehold, TaxBase = 10.00m, StartingAmount = 1000.00m, MaximumWage = 3000.00m, TaxRate = .02m };
+                yield return new TaxTable.TaxableWithholding { FilingStatus = FilingStatus.HeadOfHousehold, TaxBase = 50.00m, StartingAmount = 3000.00m, MaximumWage = 5000.00m, TaxRate = .03m };
+                yield return new TaxTable.TaxableWithholding { FilingStatus = FilingStatus.HeadOfHousehold, TaxBase = 110.00m, StartingAmount = 5000.00m, MaximumWage = 7000.00m, TaxRate = .04m };
+                yield return new TaxTable.TaxableWithholding { FilingStatus = FilingStatus.HeadOfHousehold, TaxBase = 190.00m, StartingAmount = 7000.00m, MaximumWage = 10000.00m, TaxRate = .05m };
+                yield return new TaxTable.TaxableWithholding { FilingStatus = FilingStatus.HeadOfHousehold, TaxBase = 340.00m, StartingAmount = 10000.00m, MaximumWage = Decimal.MaxValue, TaxRate = .06m };
+
+                //Married Filing Joint Return (with one spouse having income)
+
+                yield return new TaxTable.TaxableWithholding { FilingStatus = FilingStatus.MarriedWithOneIncome, TaxBase = 0.00m, StartingAmount = 0.00m, MaximumWage = 1000.00m, TaxRate = .01m };
+                yield return new TaxTable.TaxableWithholding { FilingStatus = FilingStatus.MarriedWithOneIncome, TaxBase = 10.00m, StartingAmount = 1000.00m, MaximumWage = 3000.00m, TaxRate = .02m };
+                yield return new TaxTable.TaxableWithholding { FilingStatus = FilingStatus.MarriedWithOneIncome, TaxBase = 50.00m, StartingAmount = 3000.00m, MaximumWage = 5000.00m, TaxRate = .03m };
+                yield return new TaxTable.TaxableWithholding { FilingStatus = FilingStatus.MarriedWithOneIncome, TaxBase = 110.00m, StartingAmount = 5000.00m, MaximumWage = 7000.00m, TaxRate = .04m };
+                yield return new TaxTable.TaxableWithholding { FilingStatus = FilingStatus.MarriedWithOneIncome, TaxBase = 190.00m, StartingAmount = 7000.00m, MaximumWage = 10000.00m, TaxRate = .05m };
+                yield return new TaxTable.TaxableWithholding { FilingStatus = FilingStatus.MarriedWithOneIncome, TaxBase = 340.00m, StartingAmount = 10000.00m, MaximumWage = Decimal.MaxValue, TaxRate = .06m };
+
+                // Married Filing Joint Return (both spouses having income) OR Married Filing Separate Return
+
+                yield return new TaxTable.TaxableWithholding { FilingStatus = FilingStatus.MarriedFilingSeparate, TaxBase = 0.00m, StartingAmount = 0.00m, MaximumWage = 500.00m, TaxRate = .01m };
+                yield return new TaxTable.TaxableWithholding { FilingStatus = FilingStatus.MarriedFilingSeparate, TaxBase = 5.00m, StartingAmount = 500.00m, MaximumWage = 1500.00m, TaxRate = .02m };
+                yield return new TaxTable.TaxableWithholding { FilingStatus = FilingStatus.MarriedFilingSeparate, TaxBase = 25.00m, StartingAmount = 1500.00m, MaximumWage = 2500.00m, TaxRate = .03m };
+                yield return new TaxTable.TaxableWithholding { FilingStatus = FilingStatus.MarriedFilingSeparate, TaxBase = 55.00m, StartingAmount = 2500.00m, MaximumWage = 3500.00m, TaxRate = .04m };
+                yield return new TaxTable.TaxableWithholding { FilingStatus = FilingStatus.MarriedFilingSeparate, TaxBase = 95.00m, StartingAmount = 3500.00m, MaximumWage = 5000.00m, TaxRate = .05m };
+                yield return new TaxTable.TaxableWithholding { FilingStatus = FilingStatus.MarriedFilingSeparate, TaxBase = 170.00m, StartingAmount = 5000.00m, MaximumWage = Decimal.MaxValue, TaxRate = .06m };
+
+                //Married Filing Joint Return (with both spouse having income)
+
+                yield return new TaxTable.TaxableWithholding { FilingStatus = FilingStatus.MarriedWithTwoIncomes, TaxBase = 0.00m, StartingAmount = 0.00m, MaximumWage = 500.00m, TaxRate = .01m };
+                yield return new TaxTable.TaxableWithholding { FilingStatus = FilingStatus.MarriedWithTwoIncomes, TaxBase = 5.00m, StartingAmount = 500.00m, MaximumWage = 1500.00m, TaxRate = .02m };
+                yield return new TaxTable.TaxableWithholding { FilingStatus = FilingStatus.MarriedWithTwoIncomes, TaxBase = 25.00m, StartingAmount = 1500.00m, MaximumWage = 2500.00m, TaxRate = .03m };
+                yield return new TaxTable.TaxableWithholding { FilingStatus = FilingStatus.MarriedWithTwoIncomes, TaxBase = 55.00m, StartingAmount = 2500.00m, MaximumWage = 3500.00m, TaxRate = .04m };
+                yield return new TaxTable.TaxableWithholding { FilingStatus = FilingStatus.MarriedWithTwoIncomes, TaxBase = 95.00m, StartingAmount = 3500.00m, MaximumWage = 5000.00m, TaxRate = .05m };
+                yield return new TaxTable.TaxableWithholding { FilingStatus = FilingStatus.MarriedWithTwoIncomes, TaxBase = 170.00m, StartingAmount = 5000.00m, MaximumWage = Decimal.MaxValue, TaxRate = .06m };
+            }
+        }
+    }
+}
---
+++ CertiPay.Taxes.State/NorthCarolina/TaxTable2017.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CertiPay.Taxes.State.NorthCarolina
+{
+    public class TaxTable2017 : TaxTable
+    {
+        public override int Year { get { return 2017; } }
+
+        public override Decimal SUI_Wage_Base { get { return 22300; } }
+
+        public override Decimal AllowanceValue { get { return 2500; } }
+
+        public override Decimal TaxRate { get { return 0.0585m; } }
+
+        public override Decimal StandardDeduction(FilingStatus taxStatus)
+        {
+            switch (taxStatus)
+            {
+                case FilingStatus.HeadOfHousehold:
+                    return 12400;
+
+                default:
+                    return 7750;
+            }
+        }
+    }
+}
