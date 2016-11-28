b2d33d60ec8f5d02c11452ae5f60e11fda66e7b4
Cameron Tait <ctait@certipay.com>
2016-11-28 14:53:55 -0500

    Added tests for 2017 SC Tax Table

---
+++ CertiPay.Taxes.State.Tests/SC/TaxTable2017Tests.cs
@@ -0,0 +1,30 @@
+﻿using CertiPay.Payroll.Common;
+using NUnit.Framework;
+using System;
+
+namespace CertiPay.Taxes.State.Tests.SC
+{
+    public class TaxTable2017Tests
+    {
+        [Test]
+        [TestCase(3000, 0, PayrollFrequency.Monthly, 182.72)]
+        [TestCase(2500, 0, PayrollFrequency.Monthly, 147.72)]
+        [TestCase(3000, 1, PayrollFrequency.Monthly, 152.21)]
+        [TestCase(3000, 2, PayrollFrequency.Monthly, 138.38)]
+        [TestCase(4000, 0, PayrollFrequency.Monthly, 252.72)]
+        [TestCase(4000, 1, PayrollFrequency.Monthly, 222.21)]
+        [TestCase(4000, 2, PayrollFrequency.Monthly, 208.38)]
+        [TestCase(300, 0, PayrollFrequency.Weekly, 14.70)]
+        [TestCase(300, 1, PayrollFrequency.Weekly, 9.41)]
+        [TestCase(300, 2, PayrollFrequency.Weekly, 6.49)]
+        [TestCase(700, 0, PayrollFrequency.Weekly, 42.7)]
+        [TestCase(700, 1, PayrollFrequency.Weekly, 35.66)]
+        [TestCase(700, 2, PayrollFrequency.Weekly, 32.47)]
+        public void Checks_And_Balances(Decimal grossWages, int exemptions, PayrollFrequency payrollFrequency, Decimal expected)
+        {
+            var result = new SouthCarolina.TaxTable2017().Calculate(grossWages, payrollFrequency, exemptions);
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
