93ced4637d55caf3ae6369ce988166ed03187acb
Matt Wagner <mattgwagner@gmail.com>
2016-12-09 13:53:07 -0500

    Update tax rate and deduction for NC for 2017

--- CertiPay.Taxes.State/NorthCarolina/TaxTable2017.cs
+++ CertiPay.Taxes.State/NorthCarolina/TaxTable2017.cs
@@ -6,21 +6,21 @@ namespace CertiPay.Taxes.State.NorthCarolina
     {
         public override int Year { get { return 2017; } }
 
-        public override Decimal SUI_Wage_Base { get { return 22300; } }
+        public override Decimal SUI_Wage_Base { get { return 23100; } }
 
         public override Decimal AllowanceValue { get { return 2500; } }
 
-        public override Decimal TaxRate { get { return 0.0585m; } }
+        public override Decimal TaxRate { get { return 0.05599m; } }
 
         public override Decimal StandardDeduction(FilingStatus taxStatus)
         {
             switch (taxStatus)
             {
                 case FilingStatus.HeadOfHousehold:
-                    return 12400;
+                    return 14000;
 
                 default:
-                    return 7750;
+                    return 8750;
             }
         }
     }
