79ea9bd1a8fa3deaeca884efdaaae7764869e426
Matt Wagner <mattgwagner@gmail.com>
2017-01-25 15:57:45 -0500

    Change from ER guide, this matches paycheck city

--- CertiPay.Taxes.State.Tests/MT/TaxTableTests.cs
+++ CertiPay.Taxes.State.Tests/MT/TaxTableTests.cs
@@ -8,7 +8,7 @@ namespace CertiPay.Taxes.State.Tests.MT
     {
         [Test]
         [TestCase(PayrollFrequency.SemiMonthly, 550, 5, 3d)]
-        [TestCase(PayrollFrequency.BiWeekly, 2950, 2, 150d)]
+        [TestCase(PayrollFrequency.BiWeekly, 2950, 2, 152d)]
         [TestCase(PayrollFrequency.Weekly, 135, 1, 2d)]
         public void Checks_And_Balances(PayrollFrequency frequency, Decimal grossWages, int allowances, Decimal expected)
         {
