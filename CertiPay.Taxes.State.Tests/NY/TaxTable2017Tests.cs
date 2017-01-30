b88effea8b52e730fe5e4ff4e96bd11b8d93b603
Matt Wagner <mattgwagner@gmail.com>
2017-01-30 14:01:35 -0500

    Missed test update

--- CertiPay.Taxes.State.Tests/NY/TaxTable2017Tests.cs
+++ CertiPay.Taxes.State.Tests/NY/TaxTable2017Tests.cs
@@ -4,13 +4,10 @@ using NUnit.Framework;
 
 namespace CertiPay.Taxes.State.Tests.NY
 {
-    using FilingStatus = NewYork.FilingStatus;
-
     [TestFixture]
     public class TaxTable2017Tests
     {
         private NewYork.TaxTable2017 ny2017 = new NewYork.TaxTable2017();
-      
 
         [Test]
         [TestCase(1500, PayrollFrequency.Monthly, Region.NewYorkState, FilingStatus.Single, 1, 32.46)]
@@ -19,7 +16,6 @@ namespace CertiPay.Taxes.State.Tests.NY
         [TestCase(1500, PayrollFrequency.Monthly, Region.Yonkers, FilingStatus.Married, 1, 30.40)]
         [TestCase(1500, PayrollFrequency.Monthly, Region.NewYorkCity, FilingStatus.Single, 1, 22.78)]
         [TestCase(1500, PayrollFrequency.Monthly, Region.NewYorkCity, FilingStatus.Married, 1, 21.48)]
-
         public void NewYorkTaxTable2017(decimal grossWages, PayrollFrequency freq, Region region, FilingStatus status, int personalAllowances, decimal expected)
         {
             Assert.AreEqual(expected, ny2017.Calculate(grossWages, freq, region, status, personalAllowances));
