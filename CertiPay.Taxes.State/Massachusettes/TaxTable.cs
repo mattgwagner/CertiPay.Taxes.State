80d1799f2b57dee965f21eb65279f6c755791171
Cameron Tait <ctait@certipay.com>
2017-02-03 11:26:21 -0500

    Added MA Tax Tables

---
+++ CertiPay.Taxes.State/Massachusettes/TaxTable.cs
@@ -0,0 +1,62 @@
+﻿using CertiPay.Payroll.Common;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace CertiPay.Taxes.State.Massachusettes
+{
+    public abstract class TaxTable : TaxTableHeader
+    {
+        public override StateOrProvince State { get { return StateOrProvince.MA; } }
+
+        public abstract decimal TaxRate { get; }
+        public abstract decimal FICAMax { get; }
+        public abstract decimal FirstExemption { get; }
+        public abstract decimal Exemption { get; }
+        public abstract decimal HoHDeduction { get; }
+        public abstract decimal BlindDeduction { get; }
+        public abstract decimal ExemptionBonus { get; }
+
+        public virtual Decimal Calculate(Decimal grossWages, PayrollFrequency frequency, int Exemptions = 1, Decimal FICADeductions = 0.00m, bool IsBlind = false, bool IsHeadOfHouseHold = false)
+        {
+
+            var taxableWages = frequency.CalculateAnnualized(grossWages);
+
+            //Subtract the amount deducted for FICA, Medica, Massachusettes, US or Railroad Retirement Systems
+            if (FICADeductions > 0.00m)
+            {
+                taxableWages -= (FICADeductions >= FICAMax ? FICAMax : FICADeductions);
+            }
+
+            taxableWages -= GetExemption(Exemptions);
+
+            var taxWithheld = taxableWages * TaxRate;
+
+            //deduction for Head of Household
+            if (IsHeadOfHouseHold)
+            {
+                taxWithheld -= HoHDeduction;
+            }           
+
+            //Deduction if Spose or EE is blind
+            if (IsBlind)
+            {
+                taxWithheld -= BlindDeduction;
+            }
+
+            return frequency.CalculateDeannualized(taxWithheld);
+        }
+        
+
+        internal virtual Decimal GetExemption(int numOfExemptions)
+        {            
+            if (numOfExemptions > 1)            
+                return (numOfExemptions * Exemption) + ExemptionBonus;            
+            else
+                return FirstExemption;
+        }
+               
+    }
+
+}
---
+++ CertiPay.Taxes.State/Massachusettes/TaxTable2017.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections.Generic;
+namespace CertiPay.Taxes.State.Massachusettes
+{
+    public class TaxTable2017 : TaxTable
+    {
+        public override int Year { get { return 2017; } }
+        public override Decimal SUI_Wage_Base { get { return 12960; } }
+
+        public override decimal BlindDeduction
+        {
+            get
+            {
+                return 112.20m;
+            }
+        }
+
+        public override decimal Exemption
+        {
+            get
+            {
+                return 1000.00m;
+            }
+        }
+
+        public override decimal HoHDeduction
+        {
+            get
+            {
+                return 122.00m;
+            }
+        }
+
+        public override decimal FICAMax
+        {
+            get
+            {
+                return 2000.00m;
+            }
+        }
+
+        public override decimal FirstExemption
+        {
+            get
+            {
+                return 4400.00m;
+            }
+        }
+
+        public override decimal TaxRate
+        {
+            get
+            {
+                return .051m;
+            }
+        }
+
+        public override decimal ExemptionBonus
+        {
+            get
+            {
+                return 3400.00m;
+            }
+        }
+    }
+}
