ad39737afa939b3f86052d6c6e85397b7d9bdf72
Matt Wagner <mattgwagner@gmail.com>
2017-02-03 15:01:01 -0500

    Use test case from guide

--- CertiPay.Taxes.State.Tests/MA/TaxTable2016Tests.cs
+++ CertiPay.Taxes.State.Tests/MA/TaxTable2016Tests.cs
@@ -9,8 +9,9 @@ namespace CertiPay.Taxes.State.Tests.MA
         private Massachusettes.TaxTable ma2016 = new Massachusettes.TaxTable();
 
         [Test]
-        [TestCase(1500, PayrollFrequency.Monthly, 1, 2000, true, true, 29.78)]
-        [TestCase(4000, PayrollFrequency.Monthly, 2, 2000, false, false, 172.55)]
+        [TestCase(350, PayrollFrequency.Weekly, 4, 26.78, false, false, 9.51)]
+        //[TestCase(1500, PayrollFrequency.Monthly, 1, 2000, true, true, 29.78)]
+        //[TestCase(4000, PayrollFrequency.Monthly, 2, 2000, false, false, 172.55)]
         public void MA2016_Checks_And_Balances(decimal grossWages, PayrollFrequency freq, int numExemptions, decimal FICADeductions, bool IsBlind, bool IsHoH, decimal expected)
         {
             Assert.AreEqual(expected, ma2016.Calculate(grossWages, freq, numExemptions, FICADeductions, IsBlind, IsHoH));
