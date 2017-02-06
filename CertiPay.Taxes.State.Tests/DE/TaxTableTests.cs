b416a93f146bd462dd4a9e1161e1c618a21cfc36
Cameron Tait <ctait@certipay.com>
2017-02-06 11:16:31 -0500

    Added Delaware Tax Tables

---
+++ CertiPay.Taxes.State.Tests/DE/TaxTableTests.cs
@@ -0,0 +1,20 @@
+﻿using CertiPay.Payroll.Common;
+using NUnit.Framework;
+using System;
+
+namespace CertiPay.Taxes.State.Tests.DE
+{
+    public class TaxTableTests
+    {
+
+        [Test]
+        [TestCase(PayrollFrequency.SemiMonthly, 1825, Delaware.FilingStatus.MarriedFilingJointly, 2, 60.99d)]
+        [TestCase(PayrollFrequency.BiWeekly, 2500, Delaware.FilingStatus.Single, 2, 109.19d)]
+        public void DE2017Checks_And_Balances(PayrollFrequency frequency, Decimal grossWages, Delaware.FilingStatus filingStatus, int allowances, Decimal expected)
+        {
+            var result = new Delaware.TaxTable2014().Calculate(grossWages, frequency, filingStatus, allowances);
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
---
+++ CertiPay.Taxes.State/Delaware/TaxTable.cs
@@ -0,0 +1,92 @@
+﻿using CertiPay.Payroll.Common;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace CertiPay.Taxes.State.Delaware
+{
+    public abstract class TaxTable : TaxTableHeader
+    {
+        public override StateOrProvince State { get { return StateOrProvince.DE; } }
+
+        public abstract IEnumerable<StandardDeduction> StandardDeductions { get; }
+
+        public abstract decimal PersonalAllowanceValue { get; }
+        
+        public abstract IEnumerable<TaxableWithholding> TaxableWithholdings { get; }
+
+        public virtual Decimal Calculate(Decimal grossWages, PayrollFrequency frequency, FilingStatus filingStatus = FilingStatus.Single, int personalAllowances = 1)
+        {
+            var taxableWages = frequency.CalculateAnnualized(grossWages);                        
+
+            taxableWages -= GetStandardDeduction(filingStatus);            
+
+            var selected_row = GetTaxWithholding(taxableWages);            
+
+            var taxWithheld = selected_row.TaxBase + ((taxableWages - selected_row.StartingAmount) * selected_row.TaxRate);
+
+            taxWithheld -= GetPersonalAllowance(personalAllowances);
+            
+            return frequency.CalculateDeannualized(taxWithheld);
+        }
+
+        internal virtual Decimal GetStandardDeduction(FilingStatus filingStatus)
+        {
+            return
+                StandardDeductions
+                .Where(d => d.FilingStatus == filingStatus)
+                .Select(d => d.Amount)
+                .Single();
+        }
+
+        internal virtual Decimal GetPersonalAllowance(int personalAllowances = 1)
+        {                               
+            return PersonalAllowanceValue * personalAllowances;
+        }
+      
+
+        internal virtual TaxableWithholding GetTaxWithholding(Decimal taxableWages)
+        {            
+            return
+                TaxableWithholdings                
+                .Where(d => d.StartingAmount <= taxableWages)
+                .Where(d => taxableWages < d.MaximumWage)
+                .Select(d => d)
+                .Single();
+        }
+
+        public class StandardDeduction
+        {
+            public FilingStatus FilingStatus { get; set; }
+            public Decimal Amount { get; set; }
+        }       
+
+        public class TaxableWithholding
+        {            
+            public Decimal TaxBase { get; set; }
+
+            public Decimal StartingAmount { get; set; }
+
+            public Decimal MaximumWage { get; set; }
+
+            public Decimal TaxRate { get; set; }
+        }
+    }
+
+    public enum FilingStatus : byte
+    {
+        // A) Single
+        // B) Married - 2 incomes
+        // C) Married - 1 income        
+
+        [Display(Name = "A - Single")]
+        Single = 0,
+
+        [Display(Name = "B - Married Filing Jointly")]
+        MarriedFilingJointly = 1,
+
+        [Display(Name = "C - Married Filing Seperately")]
+        MarriedFilingSeperate = 2,        
+    }
+}
