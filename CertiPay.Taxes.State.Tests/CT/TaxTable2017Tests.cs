919d35b895e689e379f102f58d4a5f341564796b
Matt Wagner <mattgwagner@gmail.com>
2017-02-14 11:49:25 -0500

    Tweak CT test to get the table from the lookup

--- CertiPay.Taxes.State.Tests/CT/TaxTable2017Tests.cs
+++ CertiPay.Taxes.State.Tests/CT/TaxTable2017Tests.cs
@@ -1,21 +1,15 @@
 ﻿using CertiPay.Payroll.Common;
 using NUnit.Framework;
 
-
 namespace CertiPay.Taxes.State.Tests.CT
 {
-    
-
     [TestFixture]
     public class TaxTable2017Tests
     {
-        private Connecticut.TaxTable2017 ct2017 = new Connecticut.TaxTable2017();
-        
         //verified with PCC unless noted otherwise
         [Test]
         //verified by hand
         [TestCase(1500, PayrollFrequency.Monthly, Connecticut.TaxTable.WithholdingCode.A, 1, 9.00)]
-
         [TestCase(1500, PayrollFrequency.Monthly, Connecticut.TaxTable.WithholdingCode.B, 1, 0.00)]
         [TestCase(1500, PayrollFrequency.Monthly, Connecticut.TaxTable.WithholdingCode.C, 1, 0.00)]
         [TestCase(1500, PayrollFrequency.Monthly, Connecticut.TaxTable.WithholdingCode.D, 1, 58.33)]
@@ -28,7 +22,11 @@ namespace CertiPay.Taxes.State.Tests.CT
         [TestCase(8000, PayrollFrequency.Monthly, Connecticut.TaxTable.WithholdingCode.F, 1, 415.83)]
         public void CT_2017_Checks_And_Balances(decimal grossWages, PayrollFrequency freq, Connecticut.TaxTable.WithholdingCode employeeCode, int personalAllowances, decimal expected)
         {
-            Assert.AreEqual(expected, ct2017.Calculate(grossWages, freq, employeeCode, personalAllowances));
+            var table = TaxTables.GetForState(StateOrProvince.CT, year: 2017) as Connecticut.TaxTable;
+
+            var result = table.Calculate(grossWages, freq, employeeCode, personalAllowances);
+
+            Assert.AreEqual(expected, result);
         }
     }
 }
