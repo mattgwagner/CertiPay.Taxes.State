5edc4bcdfcdf3ed7fdb665c1ddcc4a7673ab08c1
Cameron Tait <ctait@certipay.com>
2017-02-15 14:29:25 -0500

    Changed to enumerables

--- CertiPay.Taxes.State/Arizona/TaxTable.cs
+++ CertiPay.Taxes.State/Arizona/TaxTable.cs
@@ -9,13 +9,53 @@ namespace CertiPay.Taxes.State.Arizona
     public abstract class TaxTable : TaxTableHeader
     {
         public override StateOrProvince State { get; internal set; } = StateOrProvince.AZ;
-
-        public abstract IEnumerable<Decimal> WithholdingRates { get; }
-
-        public virtual Decimal Calculate(Decimal grossWages, Decimal rate)
+        public override Decimal SUI_Wage_Base { get { return 7000; } }        
+        public virtual Decimal Calculate(Decimal grossWages, TaxRate taxRate)
         {
-            return grossWages * rate;
+            return grossWages * GetTaxRate(taxRate);
         }
+
+        public decimal GetTaxRate(TaxRate taxRate)
+        {
+            if (taxRate == TaxRate.zeroeight)
+                return 0.008m;
+            else if (taxRate == TaxRate.onethree)
+                return 0.013m;
+            else if (taxRate == TaxRate.oneeight)
+                return 0.018m;
+            else if (taxRate == TaxRate.twoseven)
+                return 0.027m;
+            else if (taxRate == TaxRate.threesix)
+                return 0.036m;
+            else if (taxRate == TaxRate.fourtwo)
+                return 0.042m;
+            else if (taxRate == TaxRate.fiveone)
+                return 0.051m;
+            else
+                return 0.00m;                 
+            }
+        }
+
+
+        public enum TaxRate : Byte
+        {
+            [Display(Name = "0.080%")]
+            zeroeight = 0,
+            [Display(Name = "0.013%")]
+            onethree = 1,
+            [Display(Name = "0.018%")]
+            oneeight = 2,
+            [Display(Name="0.027%")]
+            twoseven = 3,
+            [Display(Name="0.036%")]
+            threesix = 4, 
+            [Display(Name="0.042%")]
+            fourtwo = 5,
+            [Display(Name="0.051%")]
+            fiveone = 6
+            
+        }
+        
 
     }
 }
