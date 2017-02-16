fff9011db74cce2bee4cc6673512531be3de1030
Matt Wagner <mattgwagner@gmail.com>
2017-02-16 13:21:44 -0500

    Add two more test cases for Arkansas

--- CertiPay.Taxes.State.Tests/AK/TaxTable2017Tests.cs
+++ CertiPay.Taxes.State.Tests/AK/TaxTable2017Tests.cs
@@ -5,14 +5,15 @@ namespace CertiPay.Taxes.State.Tests.AK
 {
     [TestFixture]
     public class TaxTable2017Tests
-    {       
-         
-        [Test]        
-        [TestCase(2127, PayrollFrequency.Monthly, 2, 61.35)]      
+    {
+        [Test]
+        [TestCase(2127, PayrollFrequency.Monthly, 2, 61.35)]
+        [TestCase(1250, PayrollFrequency.Weekly, 2, 64.25)]
+        [TestCase(954, PayrollFrequency.Weekly, 1, 44.38)]
         public void AR_2017_Checks_And_Balances(decimal grossWages, PayrollFrequency frequency, int exemptions, decimal expected)
         {
-            var taxTable = TaxTables.GetForState(StateOrProvince.AR, 2017) as Arkansas.TaxTable;
-            
+            var taxTable = TaxTables.GetForState(StateOrProvince.AR, year: 2017) as Arkansas.TaxTable;
+
             var result = taxTable.Calculate(grossWages, frequency, exemptions);
 
             Assert.AreEqual(expected, result);
