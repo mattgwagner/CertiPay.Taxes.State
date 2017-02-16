2012ab90e8b64e1b72bccdc57ef977687b503143
Cameron Tait <ctait@certipay.com>
2017-02-16 11:21:38 -0500

    Added Idaho Tax Tables

---
+++ CertiPay.Taxes.State/Idaho/TaxTable.cs
@@ -0,0 +1,76 @@
+﻿using CertiPay.Payroll.Common;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace CertiPay.Taxes.State.Idaho
+{
+    public abstract class TaxTable : TaxTableHeader
+    {
+        public override StateOrProvince State { get { return StateOrProvince.ID; } }
+
+        public abstract Decimal AnnualAllowance{ get; }        
+        
+        public abstract IEnumerable<TaxableWithholding> TaxableWithholdings { get; }
+
+        public virtual Decimal Calculate(Decimal grossWages, PayrollFrequency frequency, FilingStatus filingStatus = FilingStatus.Single, int personalAllowances = 1)
+        {
+            var taxableWages = frequency.CalculateAnnualized(grossWages);
+            
+            taxableWages -= GetPersonalAllowance(personalAllowances);
+
+            var selected_row = GetTaxWithholding(filingStatus, taxableWages);            
+
+            var taxWithheld = Math.Round(selected_row.TaxBase + ((taxableWages - selected_row.StartingAmount) * selected_row.TaxRate), MidpointRounding.AwayFromZero);            
+
+            return Math.Round(frequency.CalculateDeannualized(taxWithheld), MidpointRounding.AwayFromZero);
+        }
+        
+
+        internal virtual Decimal GetPersonalAllowance(int personalAllowances = 1)
+        {            
+            return AnnualAllowance * personalAllowances;
+        }
+        
+        internal virtual TaxableWithholding GetTaxWithholding(FilingStatus filingStatus, Decimal taxableWages)
+        {
+            if (taxableWages < Decimal.Zero) return new TaxableWithholding { };
+
+            return
+                TaxableWithholdings
+                .Where(d => d.FilingStatus == filingStatus)
+                .Where(d => d.StartingAmount <= taxableWages)
+                .Where(d => taxableWages < d.MaximumWage)
+                .Select(d => d)
+                .Single();
+        }
+        
+
+        public class TaxableWithholding
+        {
+            public FilingStatus FilingStatus { get; set; } = FilingStatus.Single;
+
+            public Decimal TaxBase { get; set; }
+
+            public Decimal StartingAmount { get; set; }
+
+            public Decimal MaximumWage { get; set; }
+
+            public Decimal TaxRate { get; set; }
+        }
+    }
+
+    public enum FilingStatus : byte
+    {
+
+        [Display(Name = "Single")]
+        Single = 0,
+
+        [Display(Name = "Married")]
+        Married = 1,
+        
+        [Display(Name = "Head of Household")]
+        HeadOfHousehold = 2
+    }
+}
--- CertiPay.Taxes.State/TaxTable2017.cs
+++ CertiPay.Taxes.State/TaxTable2017.cs
@@ -23,7 +23,7 @@ namespace CertiPay.Taxes.State
                 yield return new TaxTableHeader { Year = Year, State = StateOrProvince.FL, SUI_Wage_Base = 7000 }; // Florida
                 yield return new Georgia.TaxTable2017 { }; // Georgia
                 yield return new TaxTableHeader { Year = Year, State = StateOrProvince.HI, SUI_Wage_Base = 44000 }; // Hawaii
-                yield return new TaxTableHeader { Year = Year, State = StateOrProvince.ID, SUI_Wage_Base = 37800 }; // Idaho
+                yield return new Idaho.TaxTable2017 { };
                 yield return new Illinois.TaxTable2017 { }; // Illinios
                 yield return new TaxTableHeader { Year = Year, State = StateOrProvince.IN, SUI_Wage_Base = 9500 }; // Indiana
                 yield return new TaxTableHeader { Year = Year, State = StateOrProvince.IA, SUI_Wage_Base = 29300 }; // Iowa
