fd8c1e77ef441c48616998f49b0cf0b5624efbfc
Cameron Tait <ctait@certipay.com>
2017-02-23 15:07:36 -0500

    added New Mexico SWT

---
+++ CertiPay.Taxes.State.Tests/NM/TaxTable2017Tests.cs
@@ -0,0 +1,23 @@
+﻿using CertiPay.Payroll.Common;
+using NUnit.Framework;
+
+namespace CertiPay.Taxes.State.Tests.NM
+{
+    [TestFixture]
+    public class TaxTable2017Tests
+    {
+        [Test]
+        //Verified with PCC
+        [TestCase(2205, PayrollFrequency.BiWeekly, FilingStatus.Married, 2, 60.79)]
+        [TestCase(2205, PayrollFrequency.BiWeekly, FilingStatus.Single, 0, 93.60)]
+        [TestCase(5, PayrollFrequency.BiWeekly, FilingStatus.Married, 2, 0)]
+        public void NewMexico_2017_Checks_And_Balances(decimal grossWages, PayrollFrequency freq, FilingStatus filingStatus, int personalAllowances, decimal expected)
+        {
+            var table = TaxTables.GetForState(StateOrProvince.NM, year: 2017) as NewMexico.TaxTable2017;
+
+            var result = table.Calculate(grossWages, freq, filingStatus, personalAllowances);
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
