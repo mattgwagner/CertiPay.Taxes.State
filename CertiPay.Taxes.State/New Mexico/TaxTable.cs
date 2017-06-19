a2ddfed8a98e317dd65890ce4eb276fb2c796bf2
Matt Wagner <mattgwagner@gmail.com>
2017-06-19 14:19:01 -0400

    Fix formatting and protection levels on NM

--- CertiPay.Taxes.State/New Mexico/TaxTable.cs
+++ CertiPay.Taxes.State/New Mexico/TaxTable.cs
@@ -1,41 +1,40 @@
 ﻿using CertiPay.Payroll.Common;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 
 namespace CertiPay.Taxes.State.NewMexico
 {
     public abstract class TaxTable : TaxTableHeader
     {
-        public override StateOrProvince State { get { return StateOrProvince.NM; } }        
+        public override StateOrProvince State { get { return StateOrProvince.NM; } }
 
-        protected abstract decimal PersonalAllowances { get; }        
+        protected abstract decimal PersonalAllowances { get; }
+
         protected abstract IEnumerable<TaxableWithholding> TaxableWithholdings { get; }
 
         public virtual Decimal Calculate(Decimal grossWages, PayrollFrequency frequency, FilingStatus filingStatus = FilingStatus.Single, int personalAllowances = 1)
         {
-            var taxableWages = frequency.CalculateAnnualized(grossWages);    
+            var taxableWages = frequency.CalculateAnnualized(grossWages);
 
-            taxableWages -= GetPersonalAllowance(personalAllowances);                                    
+            taxableWages -= GetPersonalAllowance(personalAllowances);
 
-            var selected_row = GetTaxWithholding(filingStatus, taxableWages);            
+            var selected_row = GetTaxWithholding(filingStatus, taxableWages);
 
-            var taxWithheld = selected_row.TaxBase + ((taxableWages - selected_row.StartingAmount) * selected_row.TaxRate);            
+            var taxWithheld = selected_row.TaxBase + ((taxableWages - selected_row.StartingAmount) * selected_row.TaxRate);
 
             return frequency.CalculateDeannualized(Math.Max(0, taxWithheld));
         }
-        
 
-        internal virtual Decimal GetPersonalAllowance(int personalAllowances = 1)
+        protected virtual Decimal GetPersonalAllowance(int personalAllowances = 1)
         {
             return personalAllowances * PersonalAllowances;
         }
-        
 
-        internal virtual TaxableWithholding GetTaxWithholding(FilingStatus filingStatus, Decimal taxableWages)
+        protected virtual TaxableWithholding GetTaxWithholding(FilingStatus filingStatus, Decimal taxableWages)
         {
             if (taxableWages < Decimal.Zero) return new TaxableWithholding { };
+
             return
                 TaxableWithholdings
                 .Where(d => d.FilingStatus == filingStatus)
@@ -44,9 +43,8 @@ namespace CertiPay.Taxes.State.NewMexico
                 .Select(d => d)
                 .Single();
         }
-       
 
-        public class TaxableWithholding
+        protected class TaxableWithholding
         {
             public FilingStatus FilingStatus { get; set; } = FilingStatus.Single;
 
