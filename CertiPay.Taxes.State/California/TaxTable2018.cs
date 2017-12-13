e4edae696a5a47f0f0c4361f1b4f4e7ee901c1cf
Cameron Tait <ctait@certipay.com>
2017-12-13 16:05:44 -0500

    Updated California 2018 Tax Table

--- CertiPay.Taxes.State/California/TaxTable2018.cs
+++ CertiPay.Taxes.State/California/TaxTable2018.cs
@@ -13,8 +13,8 @@ namespace CertiPay.Taxes.State.California
         {
             get
             {
-                yield return new IncomeBracket { FilingStatus = FilingStatus.Single, Amount = 13687 };
-                yield return new IncomeBracket { FilingStatus = FilingStatus.HeadOfHousehold, Amount = 27373 };
+                yield return new IncomeBracket { FilingStatus = FilingStatus.Single, Amount = 14048 };
+                yield return new IncomeBracket { FilingStatus = FilingStatus.HeadOfHousehold, Amount = 28095 };
             }
         }
 
@@ -22,7 +22,7 @@ namespace CertiPay.Taxes.State.California
         {
             get
             {
-                yield return new StandardDeduction { FilingStatus = FilingStatus.Single, Amount = 4129 };
+                yield return new StandardDeduction { FilingStatus = FilingStatus.Single, Amount = 4236 };
                 yield return new StandardDeduction { FilingStatus = FilingStatus.HeadOfHousehold, Amount = 8258 };
             }
         }
@@ -31,38 +31,38 @@ namespace CertiPay.Taxes.State.California
         {
             get
             {
-                yield return new TaxableWithholding { FilingStatus = FilingStatus.Single, StartingAmount = 0.00m, MaximumWage = 8015, TaxRate = 0.011m, TaxBase = 0.00m };
-                yield return new TaxableWithholding { FilingStatus = FilingStatus.Single, StartingAmount = 8015, MaximumWage = 19001, TaxRate = 0.022m, TaxBase = 88.17m };
-                yield return new TaxableWithholding { FilingStatus = FilingStatus.Single, StartingAmount = 19001, MaximumWage = 29989, TaxRate = 0.044m, TaxBase = 329.86m };
-                yield return new TaxableWithholding { FilingStatus = FilingStatus.Single, StartingAmount = 29989, MaximumWage = 41629, TaxRate = 0.066m, TaxBase = 813.33m };
-                yield return new TaxableWithholding { FilingStatus = FilingStatus.Single, StartingAmount = 41629, MaximumWage = 52612, TaxRate = 0.088m, TaxBase = 1581.57m };
-                yield return new TaxableWithholding { FilingStatus = FilingStatus.Single, StartingAmount = 52612, MaximumWage = 268750, TaxRate = 0.1023m, TaxBase = 2548.07m };
-                yield return new TaxableWithholding { FilingStatus = FilingStatus.Single, StartingAmount = 268750, MaximumWage = 322499, TaxRate = 0.1133m, TaxBase = 24658.99m };
-                yield return new TaxableWithholding { FilingStatus = FilingStatus.Single, StartingAmount = 322499, MaximumWage = 537498, TaxRate = 0.1243m, TaxBase = 30748.75m };
-                yield return new TaxableWithholding { FilingStatus = FilingStatus.Single, StartingAmount = 537498, MaximumWage = 1000000, TaxRate = 0.1353m, TaxBase = 57473.13m };
-                yield return new TaxableWithholding { FilingStatus = FilingStatus.Single, StartingAmount = 1000000, MaximumWage = decimal.MaxValue, TaxRate = 0.1463m, TaxBase = 120049.65m };
+                yield return new TaxableWithholding { FilingStatus = FilingStatus.Single, StartingAmount = 0.00m, MaximumWage = 8008, TaxRate = 0.011m, TaxBase = 0.00m };
+                yield return new TaxableWithholding { FilingStatus = FilingStatus.Single, StartingAmount = 8008, MaximumWage = 18980, TaxRate = 0.022m, TaxBase = 88.14m };
+                yield return new TaxableWithholding { FilingStatus = FilingStatus.Single, StartingAmount = 18980, MaximumWage = 30004, TaxRate = 0.044m, TaxBase = 329.42m };
+                yield return new TaxableWithholding { FilingStatus = FilingStatus.Single, StartingAmount = 30004, MaximumWage = 41652, TaxRate = 0.066m, TaxBase = 814.58m };
+                yield return new TaxableWithholding { FilingStatus = FilingStatus.Single, StartingAmount = 41652, MaximumWage = 52624, TaxRate = 0.088m, TaxBase = 1583.40m };
+                yield return new TaxableWithholding { FilingStatus = FilingStatus.Single, StartingAmount = 52624, MaximumWage = 268736, TaxRate = 0.1023m, TaxBase = 2549.04m };
+                yield return new TaxableWithholding { FilingStatus = FilingStatus.Single, StartingAmount = 268736, MaximumWage = 322504, TaxRate = 0.1133m, TaxBase = 24657.36m };
+                yield return new TaxableWithholding { FilingStatus = FilingStatus.Single, StartingAmount = 322504, MaximumWage = 537524, TaxRate = 0.1243m, TaxBase = 30749.16m };
+                yield return new TaxableWithholding { FilingStatus = FilingStatus.Single, StartingAmount = 537524, MaximumWage = 1000012, TaxRate = 0.1353m, TaxBase = 57476.12m };
+                yield return new TaxableWithholding { FilingStatus = FilingStatus.Single, StartingAmount = 1000012, MaximumWage = decimal.MaxValue, TaxRate = 0.1463m, TaxBase = 120050.80m };
 
-                yield return new TaxableWithholding { FilingStatus = FilingStatus.Married, StartingAmount = 0.00m, MaximumWage = 16030, TaxRate = 0.011m, TaxBase = 0.00m };
-                yield return new TaxableWithholding { FilingStatus = FilingStatus.Married, StartingAmount = 16030, MaximumWage = 38002, TaxRate = 0.022m, TaxBase = 176.33m };
-                yield return new TaxableWithholding { FilingStatus = FilingStatus.Married, StartingAmount = 38002, MaximumWage = 59978, TaxRate = 0.044m, TaxBase = 659.71m };
-                yield return new TaxableWithholding { FilingStatus = FilingStatus.Married, StartingAmount = 59978, MaximumWage = 83258, TaxRate = 0.066m, TaxBase = 1626.65m};
-                yield return new TaxableWithholding { FilingStatus = FilingStatus.Married, StartingAmount = 83258, MaximumWage = 105224, TaxRate = 0.088m, TaxBase = 3163.13m };
-                yield return new TaxableWithholding { FilingStatus = FilingStatus.Married, StartingAmount = 105224, MaximumWage = 537500, TaxRate = 0.1023m, TaxBase = 5096.14m };
-                yield return new TaxableWithholding { FilingStatus = FilingStatus.Married, StartingAmount = 537500, MaximumWage = 644998, TaxRate = 0.1133m, TaxBase = 49317.97m };
-                yield return new TaxableWithholding { FilingStatus = FilingStatus.Married, StartingAmount = 644998, MaximumWage = 1000000, TaxRate = 0.1243m, TaxBase = 61497.49m };
-                yield return new TaxableWithholding { FilingStatus = FilingStatus.Married, StartingAmount = 1000000, MaximumWage = 1074996, TaxRate = 0.1353m, TaxBase = 105624.24m };
-                yield return new TaxableWithholding { FilingStatus = FilingStatus.Married, StartingAmount = 1074966, MaximumWage = decimal.MaxValue, TaxRate = 0.1463m, TaxBase = 115771.20m };
+                yield return new TaxableWithholding { FilingStatus = FilingStatus.Married, StartingAmount = 0.00m, MaximumWage = 16016, TaxRate = 0.011m, TaxBase = 0.00m };
+                yield return new TaxableWithholding { FilingStatus = FilingStatus.Married, StartingAmount = 16016, MaximumWage = 37960, TaxRate = 0.022m, TaxBase = 176.28m };
+                yield return new TaxableWithholding { FilingStatus = FilingStatus.Married, StartingAmount = 37960, MaximumWage = 60008, TaxRate = 0.044m, TaxBase = 659.10m };
+                yield return new TaxableWithholding { FilingStatus = FilingStatus.Married, StartingAmount = 60008, MaximumWage = 83304, TaxRate = 0.066m, TaxBase = 1629.16m};
+                yield return new TaxableWithholding { FilingStatus = FilingStatus.Married, StartingAmount = 83304, MaximumWage = 105248, TaxRate = 0.088m, TaxBase = 3166.80m };
+                yield return new TaxableWithholding { FilingStatus = FilingStatus.Married, StartingAmount = 105248, MaximumWage = 537472, TaxRate = 0.1023m, TaxBase = 5097.82m };
+                yield return new TaxableWithholding { FilingStatus = FilingStatus.Married, StartingAmount = 537472, MaximumWage = 645008, TaxRate = 0.1133m, TaxBase = 49314.46m };
+                yield return new TaxableWithholding { FilingStatus = FilingStatus.Married, StartingAmount = 645008, MaximumWage = 1000012, TaxRate = 0.1243m, TaxBase = 61498.32m };
+                yield return new TaxableWithholding { FilingStatus = FilingStatus.Married, StartingAmount = 1000012, MaximumWage = 1074996, TaxRate = 0.1353m, TaxBase = 105625.30m };
+                yield return new TaxableWithholding { FilingStatus = FilingStatus.Married, StartingAmount = 1074996, MaximumWage = decimal.MaxValue, TaxRate = 0.1463m, TaxBase = 115770.70m };
 
-                yield return new TaxableWithholding { FilingStatus = FilingStatus.HeadOfHousehold, StartingAmount = 0.00m, MaximumWage = 16040, TaxRate = 0.011m, TaxBase = 0.00m };
-                yield return new TaxableWithholding { FilingStatus = FilingStatus.HeadOfHousehold, StartingAmount = 16040, MaximumWage = 38003, TaxRate = 0.022m, TaxBase = 176.44m };
-                yield return new TaxableWithholding { FilingStatus = FilingStatus.HeadOfHousehold, StartingAmount = 38003, MaximumWage = 48990, TaxRate = 0.044m, TaxBase = 659.63m };
-                yield return new TaxableWithholding { FilingStatus = FilingStatus.HeadOfHousehold, StartingAmount = 48990, MaximumWage = 60630, TaxRate = 0.066m, TaxBase = 1143.06m };
-                yield return new TaxableWithholding { FilingStatus = FilingStatus.HeadOfHousehold, StartingAmount = 60630, MaximumWage =  71615, TaxRate = 0.088m, TaxBase = 1911.30m };
-                yield return new TaxableWithholding { FilingStatus = FilingStatus.HeadOfHousehold, StartingAmount = 71615, MaximumWage = 365499, TaxRate = 0.1023m, TaxBase = 2877.98m };
-                yield return new TaxableWithholding { FilingStatus = FilingStatus.HeadOfHousehold, StartingAmount = 365499, MaximumWage = 438599, TaxRate = 0.1133m, TaxBase = 32942.31m };
-                yield return new TaxableWithholding { FilingStatus = FilingStatus.HeadOfHousehold, StartingAmount = 438599, MaximumWage =  730997, TaxRate = 0.1243m, TaxBase = 41224.54m };
-                yield return new TaxableWithholding { FilingStatus = FilingStatus.HeadOfHousehold, StartingAmount = 730997, MaximumWage = 1000000, TaxRate = 0.1353m, TaxBase = 77569.61m };
-                yield return new TaxableWithholding { FilingStatus = FilingStatus.HeadOfHousehold, StartingAmount = 1074966, MaximumWage = decimal.MaxValue, TaxRate = 0.1463m, TaxBase = 113965.72m };
+                yield return new TaxableWithholding { FilingStatus = FilingStatus.HeadOfHousehold, StartingAmount = 0.00m, MaximumWage = 16016, TaxRate = 0.011m, TaxBase = 0.00m };
+                yield return new TaxableWithholding { FilingStatus = FilingStatus.HeadOfHousehold, StartingAmount = 16016, MaximumWage = 38012, TaxRate = 0.022m, TaxBase = 176.28m };
+                yield return new TaxableWithholding { FilingStatus = FilingStatus.HeadOfHousehold, StartingAmount = 38012, MaximumWage = 48984, TaxRate = 0.044m, TaxBase = 660.14m };
+                yield return new TaxableWithholding { FilingStatus = FilingStatus.HeadOfHousehold, StartingAmount = 48984, MaximumWage = 60632, TaxRate = 0.066m, TaxBase = 1142.96m };
+                yield return new TaxableWithholding { FilingStatus = FilingStatus.HeadOfHousehold, StartingAmount = 60632, MaximumWage =  71604, TaxRate = 0.088m, TaxBase = 1911.78m };
+                yield return new TaxableWithholding { FilingStatus = FilingStatus.HeadOfHousehold, StartingAmount = 71604, MaximumWage = 365508, TaxRate = 0.1023m, TaxBase = 2877.42m };
+                yield return new TaxableWithholding { FilingStatus = FilingStatus.HeadOfHousehold, StartingAmount = 365508, MaximumWage = 438620, TaxRate = 0.1133m, TaxBase = 32943.82m };
+                yield return new TaxableWithholding { FilingStatus = FilingStatus.HeadOfHousehold, StartingAmount = 438620, MaximumWage =  731016, TaxRate = 0.1243m, TaxBase = 41227.42m };
+                yield return new TaxableWithholding { FilingStatus = FilingStatus.HeadOfHousehold, StartingAmount = 731016, MaximumWage = 1000012, TaxRate = 0.1353m, TaxBase = 77572.42m };
+                yield return new TaxableWithholding { FilingStatus = FilingStatus.HeadOfHousehold, StartingAmount = 1000012, MaximumWage = decimal.MaxValue, TaxRate = 0.1463m, TaxBase = 113967.40m };
             }
         }
     }
