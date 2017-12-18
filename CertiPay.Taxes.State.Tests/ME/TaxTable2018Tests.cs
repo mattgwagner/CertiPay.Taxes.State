cfafcccb74b54e8ef0020c0c4137cab7e2b6f362
Cameron Tait <ctait@certipay.com>
2017-12-18 16:38:47 -0500

    Fixed tables
    Added tests

---
+++ CertiPay.Taxes.State.Tests/ME/TaxTable2018Tests.cs
@@ -0,0 +1,37 @@
+﻿using CertiPay.Payroll.Common;
+using NUnit.Framework;
+using System;
+
+namespace CertiPay.Taxes.State.Tests.ME
+{
+    [TestFixture]
+    public class TaxTable2018Tests
+    {
+        [Test]
+        //Pay check city was wrong, verified by hand using examples from the withholding guide
+        
+        [TestCase(0, PayrollFrequency.Monthly, FilingStatus.Single, 1, 0)]
+        [TestCase(1, PayrollFrequency.Monthly, FilingStatus.Single, 1, 0)]
+        [TestCase(300, PayrollFrequency.Weekly, FilingStatus.Single, 2, 0)]
+        [TestCase(750, PayrollFrequency.Weekly, FilingStatus.Single, 2, 24)]
+        [TestCase(4500, PayrollFrequency.Weekly, FilingStatus.Married, 2, 284)]
+        public void Maine_2018_Checks_And_Balances(decimal grossWages, PayrollFrequency freq, FilingStatus status, int withholdingAllowances, decimal expected)
+        {
+            var table = TaxTables.GetForState(StateOrProvince.ME, year: 2018) as Maine.TaxTable;
+
+            var result = table.Calculate(grossWages, freq, status, withholdingAllowances);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        [TestCase(-1, PayrollFrequency.Monthly, FilingStatus.Single, 1)]
+        public void Maine_2018_Checks_And_Balances(decimal grossWages, PayrollFrequency freq, FilingStatus status, int withholdingAllowances)
+        {
+            var table = TaxTables.GetForState(StateOrProvince.ME, year: 2018) as Maine.TaxTable;            
+
+            Assert.Throws<ArgumentOutOfRangeException>(() => table.Calculate(grossWages, freq, status, withholdingAllowances));
+        }
+        
+    }
+}
--- CertiPay.Taxes.State/Maine/TaxTable2018.cs
+++ CertiPay.Taxes.State/Maine/TaxTable2018.cs
@@ -20,8 +20,8 @@ namespace CertiPay.Taxes.State.Maine
                 yield return new StandardDeduction { FilingStatus = FilingStatus.Single, FloorAmount = 146100.00m, CeilingAmount = decimal.MaxValue, CalcValue = 0.00m, Amount = 0.00m };
 
                 yield return new StandardDeduction { FilingStatus = FilingStatus.Married, FloorAmount = 0.00m, CeilingAmount = 142200.00m, CalcValue = 0.00m, Amount = 20750.00m };
-                yield return new StandardDeduction { FilingStatus = FilingStatus.Married, FloorAmount = 142200.00m, CeilingAmount = 290000.00m, CalcValue = 150000.00m, Amount = 20750.00m };
-                yield return new StandardDeduction { FilingStatus = FilingStatus.Married, FloorAmount = 290000.00m, CeilingAmount = decimal.MaxValue, CalcValue = 0.00m, Amount = 0.00m };
+                yield return new StandardDeduction { FilingStatus = FilingStatus.Married, FloorAmount = 142200.00m, CeilingAmount = 292200.00m, CalcValue = 150000.00m, Amount = 20750.00m };
+                yield return new StandardDeduction { FilingStatus = FilingStatus.Married, FloorAmount = 292200.00m, CeilingAmount = decimal.MaxValue, CalcValue = 0.00m, Amount = 0.00m };
             }
         }
 
@@ -30,7 +30,7 @@ namespace CertiPay.Taxes.State.Maine
             get
             {
                 yield return new TaxableWithholding { FilingStatus = FilingStatus.Single, StartingAmount = 0.00m, MaximumWage = 21450.00m, TaxBase = 0.00m, TaxRate = 0.0580m };
-                yield return new TaxableWithholding { FilingStatus = FilingStatus.Single, StartingAmount = 21450.00m, MaximumWage = 50750.00m, TaxBase = 1224.00m, TaxRate = 0.0675m };
+                yield return new TaxableWithholding { FilingStatus = FilingStatus.Single, StartingAmount = 21450.00m, MaximumWage = 50750.00m, TaxBase = 1244.00m, TaxRate = 0.0675m };
                 yield return new TaxableWithholding { FilingStatus = FilingStatus.Single, StartingAmount = 50750.00m, MaximumWage = decimal.MaxValue, TaxBase = 3222.00m, TaxRate = 0.0715m };                
 
                 yield return new TaxableWithholding { FilingStatus = FilingStatus.Married, StartingAmount = 0.00m, MaximumWage = 42900.00m, TaxBase = 0.00m, TaxRate = 0.058m };
